0653fbc78613e8d73424bb0d7f4c5d3429729d8c
Spring-Rebirth <cjunwei6249@gmail.com>
2025-06-19 15:41:27 +0800

    修复在ts文件中使用js语法的错误

--- src/navigation/RootBottomTabs.tsx
+++ src/navigation/RootBottomTabs.tsx
@@ -1,15 +1,15 @@
+import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from '../screens/root-bottom-tabs/HomeScreen';
 import ProfileScreen from '../screens/root-bottom-tabs/ProfileScreen';
-import { EvilIcons } from '@react-native-vector-icons/evil-icons';
 import { BottomTabBar } from '@react-navigation/bottom-tabs';
 
 const RootBottomTabs = createBottomTabNavigator({
-  // tabBar: (props) => <BottomTabBar { ...props } />,
+  tabBar: (props) => <BottomTabBar {...props} />,
   screens: {
     Home: HomeScreen,
     Profile: ProfileScreen,
   },
 });
 
-export default RootBottomTabs;
+export default RootBottomTabs; 
