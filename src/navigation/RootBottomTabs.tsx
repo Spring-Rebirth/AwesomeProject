9e4c1ec76ba02380f420647e963cc8fe28bacc19
Spring-Rebirth <cjunwei6249@gmail.com>
2025-06-19 17:49:34 +0800

    配置@react-native-vector-icons

--- src/navigation/RootBottomTabs.tsx
+++ src/navigation/RootBottomTabs.tsx
@@ -2,13 +2,26 @@ import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from '../screens/root-bottom-tabs/HomeScreen';
 import ProfileScreen from '../screens/root-bottom-tabs/ProfileScreen';
-import { BottomTabBar } from '@react-navigation/bottom-tabs';
+import Icon from '@react-native-vector-icons/fontawesome6';
 
 const RootBottomTabs = createBottomTabNavigator({
-  tabBar: (props) => <BottomTabBar {...props} />,
   screens: {
-    Home: HomeScreen,
-    Profile: ProfileScreen,
+    Home: {
+      screen: HomeScreen,
+      options: {
+        tabBarIcon: () => (
+          <Icon name="house" size={24} color="black" iconStyle="solid" />
+        ),
+      },
+    },
+    Profile: {
+      screen: ProfileScreen,
+      options: {
+        tabBarIcon: () => (
+          <Icon name="user" size={24} color="black" iconStyle="solid" />
+        ),
+      },
+    },
   },
 });
 
