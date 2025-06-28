4fb238f065c548554e96d5fc93fa0d7e4c1886d1
myst-seed <cjunwei6249@gmail.com>
2025-06-29 03:22:56 +0800

    为底部标签导航添加自定义TabBar组件，并优化图标属性传递

--- src/navigation/RootBottomTabs.tsx
+++ src/navigation/RootBottomTabs.tsx
@@ -1,22 +1,26 @@
 import React from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createBottomTabNavigator, BottomTabBar, type BottomTabBarProps } from '@react-navigation/bottom-tabs';
 import HomeScreen from '../screens/root-bottom-tabs/HomeScreen';
 import ProfileScreen from '../screens/root-bottom-tabs/ProfileScreen';
 import Icon from '@react-native-vector-icons/fontawesome6';
 
 const Tab = createBottomTabNavigator();
 
+const TabBar = (props: BottomTabBarProps) => (
+    <BottomTabBar {...props} />
+);
+
 const HomeTabIcon = (props: { focused: boolean, color: string, size: number }) => (
-    <Icon name="house" iconStyle='solid' color={props.color} size={props.size} />
+    <Icon name="house" iconStyle='solid' {...props} />
 );
 
 const ProfileTabIcon = (props: { focused: boolean, color: string, size: number }) => (
-    <Icon name="user" iconStyle='solid' color={props.color} size={props.size} />
+    <Icon name="user" iconStyle='solid' {...props} />
 );
 
 function RootBottomTabs() {
     return (
-        <Tab.Navigator>
+        <Tab.Navigator tabBar={TabBar}>
             <Tab.Screen
                 name="Home"
                 component={HomeScreen}
